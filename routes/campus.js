23dd980c2751dd3be631e077b8902b5bcced1660
Tess Monical <tess.monical@gmail.com>
2017-08-28 08:31:52 -0500

    Added routes for students and campuses

---
+++ routes/campus.js
@@ -0,0 +1,51 @@
+const router = require('express').Router()
+const { Campus } = require('../db')
+
+router.get('/', (req, res, next) => {
+  Campus.findAll()
+    .then((campuss) => {
+      res.json(campuss);
+    })
+    .catch(next);
+})
+
+router.get('/:campusId', (req, res, next) => {
+  Campus.findById(req.params.campusId)
+    .then((campus) => {
+      res.json(campus);
+    })
+    .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+  Campus.create(req.body)
+    .then((campus) => {
+      res.json(campus);
+    })
+    .catch(next);
+})
+
+router.put('/campusId', (req, res, next) => {
+  Campus.findById(req.params.campusId)
+    .then((campus) => {
+      return campus.update(req.body);
+    })
+    .then(campus => {
+      res.json(campus);
+    })
+    .catch(next);
+})
+
+router.delete('/:campusId', (req, res, next) => {
+  Campus.findById(req.params.campusId)
+    .then((campus) => {
+      return campus.delete();
+    })
+    .then( () => {
+      res.sendStatus(200);
+    })
+    .catch(next);
+})
+
+
+export default router;
---
+++ routes/student.js
@@ -0,0 +1,51 @@
+const router = require('express').Router()
+const { Student } = require('../db')
+
+router.get('/', (req, res, next) => {
+  Student.findAll()
+    .then((students) => {
+      res.json(students);
+    })
+    .catch(next);
+})
+
+router.get('/:studentId', (req, res, next) => {
+  Student.findById(req.params.studentId)
+    .then((student) => {
+      res.json(student);
+    })
+    .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+  Student.create(req.body)
+    .then((student) => {
+      res.json(student);
+    })
+    .catch(next);
+})
+
+router.put('/studentId', (req, res, next) => {
+  Student.findById(req.params.studentId)
+    .then((student) => {
+      return student.update(req.body);
+    })
+    .then(student => {
+      res.json(student);
+    })
+    .catch(next);
+})
+
+router.delete('/:studentId', (req, res, next) => {
+  Student.findById(req.params.studentId)
+    .then((student) => {
+      return student.delete();
+    })
+    .then( () => {
+      res.sendStatus(200);
+    })
+    .catch(next);
+})
+
+
+export default router;
